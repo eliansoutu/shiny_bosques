d537d8e8781785b442d2770624069c33c368c85d
eliansoutu <eliansoutul@gmail.com>
2024-05-20 22:03:41 -0300

    update

--- server.R
+++ server.R
@@ -1,27 +1,10 @@
 
 shinyServer(function(input, output, session) {
 
-  
   #### INFO ####
   #Render mapa
   output$mapaResumen <- renderHighchart({
     
-    #Genero mapa
-    mapa <- data %>% 
-      filter(anio == 2020, indicator_name == "Cobertura") %>% 
-      hcmap(map = "custom/world", 
-            joinBy = c("iso-a3","country_code"), 
-            value = "valor",
-            name = "Cobertura de bosques (%)",
-            dataLabels = list(enabled = TRUE, format = '{country}'),
-            download_map_data = T,
-            nullColor = "#DBDBDC",
-            borderColor = "#FAFAFA", 
-            borderWidth = 0.5) %>%
-      hc_colorAxis(minColor = "lightgreen",
-                   maxColor = "darkgreen") %>% 
-      hc_mapNavigation(enabled = T) %>%
-      hc_legend(title = list(text = "% cobertura de los bosques sobre el total de la tierra", align = "Center"))
     mapa
     
   })
